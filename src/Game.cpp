bf2b998714692e270c4a4dde4f5effedf55e966b
S0uf12n8 <oulkadisoufiane69@gmail.com>
2026-05-11 11:43:30 +0100

    game logic , and here initial class

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,40 +1,37 @@
 #include "Game.h"
+#include "Hero.h"
+#include "Colors.h"
 #include <iostream>
 #include <limits>
 #include <cstdlib>
 using namespace std;
 
+Game::Game() : running(true) {}
+Game::~Game() = default;
 
-Game::Game(): running(true){}
-
-void Game::showMainMenu(){
-   #ifdef _WIN32 
+void Game::showMainMenu() {
+    #ifdef _WIN32
         system("cls");
-   #esle 
+    #else
         system("clear");
-   #endif
+    #endif
 
-   cout << WHITE     // pick a color from Colors.h
-         << "  ================================" << "\n"
-         << "         Ragnarok                 " << "\n"  // game name here
-         << "  ================================" << "\n"
+    cout << WHITE
+         << "  ================================\n"
+         << "           Ragnarok               \n"
+         << "  ================================\n"
          << RESET << "\n";
-    cout << BOLD << "  [1]" << RESET << " New Game" << "\n";  
-    cout << BOLD << "  [2]" << RESET << " Load Game" << "\n";  
-    cout << BOLD << "  [3]" << RESET << " Exit" << "\n";  
 
+    cout << BOLD << "  [1]" << RESET << " New Game\n";
+    cout << BOLD << "  [2]" << RESET << " Load Game\n";
+    cout << BOLD << "  [3]" << RESET << " Exit\n";
     cout << "\n > ";
 }
 
-
-
-
-void Game::run(){
-     while (running) {          // what keeps the loop going?
-        
-        showMainMenu();               // show the menu first
-        
-        int choice = getUserChoice(1, 3);   // get player input between 1 and 3
+void Game::run() {
+    while (running) {
+        showMainMenu();
+        int choice = getUserChoice(1, 3);
 
         switch (choice) {
             case 1:
@@ -46,44 +43,31 @@ void Game::run(){
                 break;
             case 3:
                 cout << YELLOW << "\n  Skål! Until next time.\n" << RESET;
-                running = false;   
+                running = false;
                 break;
         }
     }
 }
 
-
-int Game::getUserChoice(int min, int max){
-    int choice;
-
-    while (true)
-    {
-        cin>>choice;
-
-        if(cin.fail() || choice < min || choice > max){
-            cin.clear();
-             cin.ignore(                     
-                numeric_limits<streamsize>::max(), '\n'
-             
-            );
-            cout << "  Invalid. Enter "<< min << "-" << max<< ": ";
-        }
-        else {
-            cin.ignore(                     
-                numeric_limits<streamsize>::max(), '\n'
-            );
-            return choice;                  
-        }
-    
-    }
-}
-
 void Game::selectHeroClass() {
-    
     cout << CYAN << "\n  [Hero selection coming soon]\n" << RESET;
 }
 
 void Game::gameLoop() {
-    
     cout << CYAN << "\n  [Game loop coming soon]\n" << RESET;
+}
+
+int Game::getUserChoice(int min, int max) {
+    int choice;
+    while (true) {
+        cin >> choice;
+        if (cin.fail() || choice < min || choice > max) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "  Invalid. Enter " << min << "-" << max << ": ";
+        } else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return choice;
+        }
+    }
 }
