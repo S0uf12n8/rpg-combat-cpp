0ecee445b4ff6612b30eb7c50d7bfdc55e959450
Asso-uab <assouabhamid1@gmail.com>
2026-05-12 11:14:09 +0100

    cahnger code item.h et cree item.cpp

--- include/Item.h
+++ include/Item.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <string>
 
-class Entity; 
+// Item is the "parent" of items in the game
+//  Consumable, Weapon, or Armor (LWLAD)
 
 class Item {
-public:
-    virtual ~Item() = default;
-
-    virtual void use(Entity& target) = 0;
-    virtual std::string getName() const = 0;
-    virtual std::string getDescription() const = 0;
-    virtual int getValue() const = 0;
-
 protected:
-    std::string name;
+    std::string name;    
     std::string description;
     int value;
+
+public:
+    Item(const std::string& name, const std::string& description, int value);
+
+    // garanti que le destructeur  est appele lorsque nous supprimons un enfant
+    virtual ~Item() = default;
+
+    // use() is what happens when the player "uses" this item
+    virtual void use() = 0;
+
+    virtual std::string getDescription() const;
+
+    std::string getName()  const;
+    int         getValue() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Item& item);
 };
