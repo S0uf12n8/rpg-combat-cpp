d9a547ccb9332c003890bf15769d9017c459ba7d
Anas EL KHADIRI <el.khadiri.anas.pro@gmail.com>
2026-05-19 10:48:44 +0100

    Implement Archer class with Precise Shot skill

--- src/Archer.cpp
+++ src/Archer.cpp
@@ -1,41 +1,36 @@
 #include "Archer.h"
 #include <iostream>
-#include <cstdlib>
+
 using namespace std;
 
-PiercingArrow::PiercingArrow()
-    : Skill("piercing arrow", 20, 40, "fast arrow with crit chance.")
-{}
-
-void PiercingArrow::use(Entity& caster, Entity& target) const {
-    cout << caster.getName() << " fires a piercing arrow!\n";
-    bool crit = (rand() % 100) < 30;
-    int dmg = crit ? power_ * 2 : power_;
-    if (crit) cout << "  *** crit hit! ***\n";
-    target.takeDamage(dmg);
-}
-
 Archer::Archer(const string& name)
-    : Hero(name, Stats(110, 26, 9, 18, 60)) {
-    skills_.push_back(new PiercingArrow());
-}
+    : Hero(name, Stats(90, 14, 8, 11, 40)) {}
 
-void Archer::attack(Entity& target) {
-    bool crit = (rand() % 100) < 20;
-    int dmg = crit ? stats_.getAttack() * 2 : stats_.getAttack();
-    cout << name_ << " fires quick shot at " << target.getName() << "!";
-    if (crit) cout << " *** crit! ***";
-    cout << "\n";
-    target.takeDamage(dmg);
+string Archer::getType() const {
+    return "Archer";
 }
 
 void Archer::useSkill(Entity& target) {
-    tryUseSkill(0, target);
+    if (stats_.getMana() < 15) {
+        cout << "Not enough mana!\n";
+        return;
+    }
+
+    cout << name_ << " uses Precise Shot!\n";
+    stats_.spendMana(15);
+
+    // Deal stats_.getAttack() damage, completely ignoring target's defense
+    target.takeDamage(stats_.getAttack() + target.getStats().getDefense());
 }
 
 void Archer::levelUp() {
     Hero::levelUp();
+    stats_.setMaxHP(stats_.getMaxHP() + 20);
+    stats_.heal(20);
+    stats_.setAttack(stats_.getAttack() + 3);
+    stats_.setDefense(stats_.getDefense() + 2);
     stats_.setSpeed(stats_.getSpeed() + 3);
-    stats_.setAttack(stats_.getAttack() + 4);
-    cout << name_ << "'s reflexes sharpened!\n";
+    stats_.setMaxMana(stats_.getMaxMana() + 5);
+    stats_.restoreMana(5);
+    cout << name_ << " leveled up! Quick and deadly!\n";
 }
