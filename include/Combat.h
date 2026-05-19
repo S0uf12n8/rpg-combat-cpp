fac5c1ac3490c5b1ebd0f9afc4b34db229a769fe
oussaelber <oussamaelberji@gmail.com>
2026-05-19 13:37:46 +0100

    last push

--- include/Combat.h
+++ include/Combat.h
@@ -1,20 +1,26 @@
 #pragma once
+#include <vector>
+#include"StatusEffect.h"
 
 class Hero;
+class Enemy;
 class Entity;
 
 class Combat{
-public:
-    Combat(Hero& hero,Entity& enemy);
-
-    void startCombat();
-    void heroTurn();
-    void enemyTurn();
-    void applyEffects();
-    bool isCombatOver();
-
 private:
     Hero& hero;
-    Entity& enemy;
-    int maxRounds;
+    Enemy& enemy;
+    std::vector<StatusEffect*>heroEffects;
+    std::vector<StatusEffect*>enemyEffects;
+    bool heroStunned;
+    bool enemyStunned;
+    int calculateDamage(Entity& attacker, Entity& defender);
+    bool tryDodge(Entity& attacker, Entity& defender);
+    void applyEffects(Entity& target, std::vector<StatusEffect*>& effects, bool& stunned);
+    void cleanupEffects(std::vector<StatusEffect*>& effects);
+public:
+    Combat(Hero& hero,Enemy& enemy);
+    ~Combat();
+    void start();
+    bool playerWon() const;
 };
